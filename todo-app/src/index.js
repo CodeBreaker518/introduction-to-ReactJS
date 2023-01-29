25606dd241ad37dc44d817a893cbf49724ed3354
CodeBreaker <diegoperezperez518@gmail.com>
2023-01-29 12:13:14 -0600

    imported materialize

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'materialize-css/dist/css/materialize.min.css'
 
 const rootContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(rootContainer);
