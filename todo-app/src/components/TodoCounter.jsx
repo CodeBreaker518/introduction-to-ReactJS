465c17c4b63c252c76ef408d9b7a6b15b4ddc1c1
CodeBreaker <diegoperezperez518@gmail.com>
2023-01-29 01:18:17 -0600

    created & imported todocounter css

--- todo-app/src/components/TodoCounter.jsx
+++ todo-app/src/components/TodoCounter.jsx
@@ -1,6 +1,8 @@
+import '../styles/TodoCounter.css'
+
 export function TodoCounter() {
   return (
-    <h2>Has completado 2 de 3 TODO's</h2>
+    <h2 className="todo-counter">Has completado 2 de 3 TODO's</h2>
   )
 }
 
