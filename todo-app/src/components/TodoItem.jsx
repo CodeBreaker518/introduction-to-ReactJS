fd4fc190a6655f86ec19972367054cd7ecd5aa45
CodeBreaker <diegoperezperez518@gmail.com>
2023-01-29 14:04:41 -0600

    making components styles

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,11 +1,30 @@
+import '../styles/TodoItem.css'
+
 export function TodoItem(props) {
   const {text} = props
   return (
-    <li>
-      <span>C</span>
-      <p>{text}</p>
-      {/* props.text = map of fakeToDos */}
-      <span>X</span>
+    <li className="todo-list-item">
+
+      <div class="row pink darken-3 valign-wrapper">
+
+        <div class="col s9">
+          <p>
+            <label>
+              <input type="checkbox" />
+              <span className="text-item">{text}</span>
+            </label>
+          </p>
+        </div>
+
+        <div class="col s3 m2">
+          <p>
+            <a class="btn-floating btn-large waves-effect waves-light red align-right">
+              <i class="material-icons">highlight_off</i>
+            </a>
+          </p>
+        </div>
+      </div>
+
     </li>
   )
 }
--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -2,11 +2,15 @@ export function TodoList(props) {
   //destructuring object props
   const {children} = props
   return (
-   <section>
-    <ul>
-      {children}
-      {/* props.children = what's inside <TodoList>....</TodoList> */}
-    </ul>
-   </section>
+    <>
+    <div className="todo-list container">
+      <section>
+        <ul className="collection">
+          {children}
+          {/* props.children = what's inside <TodoList>....</TodoList> */}
+        </ul>
+      </section>
+    </div>
+   </>
   )
 }
